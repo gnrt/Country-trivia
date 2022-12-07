5835ec6519d1ec91a22e5255a7adde811e19cf24
Bryan davie <b.davie81@gmail.com>
2022-12-07 14:22:24 -0500

    saving for pull

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([40.5008, -74.4474], 4);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+console.log(map);

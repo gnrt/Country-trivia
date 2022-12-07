97ec1a8f50af6dd2b73d022e535bb24c8014c6b9
wdiste <Wdiste49@gmail.com>
2022-12-07 11:59:04 -0500

    Data loads from api and replaces text in HTML element

--- Assets/Js/trivia-api.js
+++ Assets/Js/trivia-api.js
@@ -8,7 +8,8 @@
 
 // ------------------------------------------------------------------------------------------
 
-var quizBody = document.querySelector('.quiz');      // document location of the trivia widget to be added
+var quizBody = document.querySelector('.question-box');     // document location of the trivia widget to be added
+var score = localStorage.getItem('score');          // store 'score' data from previous games
 
 function getApi() {
     var triviaUrl = "https://the-trivia-api.com/api/questions?limit=1";
@@ -21,6 +22,16 @@ function getApi() {
         })
         .then(function(data) {
             console.log(data);
+
+            // here we will create the quiz format
+            var questionText = document.querySelector('.question')  // link to html location to create element for question display
+            var answerButtons   // link to html location to create buttons for potential answers
+
+            //  successfully loads in question to questionText div.  need to use [0] for data array to work!!
+            questionText.textContent = data[0].question;
+            console.log(data[0].question);
+
+            // questionText.append(questionText);
         });
 
 };

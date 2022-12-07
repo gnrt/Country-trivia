f8340904b69466749bee938afe7206469bf5c4dd
Adam aronson <adamaronson@adams-mbp.lan>
2022-12-07 14:29:51 -0500

    updated html css

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,4 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
